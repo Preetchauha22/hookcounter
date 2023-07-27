40304ac1f7385b47f45823c841d32a6e1836cb26
preet chauhan <138657707+Preetchauha22@users.noreply.github.com>
2023-07-27 10:30:32 +0530

    Add files via upload

---
+++ src/hookcounter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>React Hooks Counter</h2>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Counter from './hookcounter';
+import "../src/counter.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   
+    
+
+    <Counter/>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
